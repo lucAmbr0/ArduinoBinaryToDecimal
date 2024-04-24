e421e4ae4a725fc015a78d3832ca06238099db9f
Luca <luca.ambrosonee@gmail.com>
2024-04-24 18:03:48 +0200

    setup and cleaned repo

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,67 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define b8 2
+#define b4 3
+#define b2 4
+#define b1 5
+
+#define A 7
+#define B 8
+#define C 9
+#define D 10
+#define E 11
+#define F 12
+#define G 13
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(b1, INPUT_PULLUP);
+  pinMode(b2, INPUT_PULLUP);
+  pinMode(b4, INPUT_PULLUP);
+  pinMode(b8, INPUT_PULLUP);
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+  Serial.begin(9600);
+}
+
+void resetSegments() {
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+}
+
+void write1() {
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+}
+void write2() {
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, LOW);
+  digitalWrite(G, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  resetSegments();
+  delay(500);
+  write1();
+  delay(500);
+  write2();
+  delay(500);
 }
